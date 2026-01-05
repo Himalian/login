c1d0cfebd808476621284a1d2985d72e1b5cd818
Butterblock233 <Voltage15312@outlook.com>
2026-01-05 12:17:25 +0800

    refactor: use pydantic-settings to load env

--- src/config.py
+++ src/config.py
@@ -1,13 +1,24 @@
-# pyright: strict
 from pathlib import Path
+from typing import Final
 
 from dotenv import dotenv_values
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 dotenv_path = Path(__file__).parent.parent / ".env"  # ../.env
 
 
-config: dict[str, str | None] = dotenv_values(dotenv_path)
+class Settings(BaseSettings):
+    USERNAME: Final[str]
+    PASSWORD: Final[str]
+
+    model_config = SettingsConfigDict(
+        env_file=dotenv_path, env_file_encoding="utf-8", extra="ignore"
+    )
+
+
+settings = Settings()  # pyright: ignore
 
 if __name__ == "__main__":
-    print(config.get("USERNAME"))
-    print(config.get("PASSWORD"))
+    print(dotenv_path)
+    print(settings.model_dump_json())
