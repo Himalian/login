088da8961d55a0d5f90d27e55b491c56eb03aa39
Butterblock233 <Voltage15312@outlook.com>
2026-01-05 12:18:31 +0800

    refactor: update imports

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 # pyright: strict
-from src.cmcc_login import CMCCLogin
-from src.config import config
+from src.cmcc_login import CMCCAuthenticator
+from src.config import settings
 from src.redirect import is_cmcc
 
 
-def main():
-    username: str = config.get("USERNAME") or ""
-    password: str = config.get("PASSWORD") or ""
-    if username == "" or password == "":
-        print("username or password not found")
-        return
+def main() -> None:
+    username = settings.USERNAME
+    password = settings.PASSWORD
 
-    if is_cmcc():
-        print("CMCC connected")
-        login = CMCCLogin(username, password)
-        login.run()
-    else:
+    if not is_cmcc():
         from src.dorm_login import login_dorm
 
         login_dorm(username, password)
+        return
+
+    print("Status: CMCC network detected")
+    auth = CMCCAuthenticator(username, password)
+    auth.authenticate()
 
 
 if __name__ == "__main__":
